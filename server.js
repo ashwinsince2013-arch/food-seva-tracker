64852febd52fce64870360e9de01ac939df7aa94
ashwinsince2013-arch <ashwinsince2013@gmail.com>
2026-07-14 16:21:51 -0500

    Move static file serving to the correct position

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/app", appRoutes);
+
+app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));

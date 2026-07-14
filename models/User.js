22c2b66ae088bb8bb97ed320f43286ef04359864
ashwinsince2013-arch <ashwinsince2013@gmail.com>
2026-07-14 16:23:54 -0500

    Add trim option to email field in UserSchema
    Added trim option to email field in UserSchema.

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const LogSchema = new mongoose.Schema(
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, unique: true, required: true, lowercase: true },
+  email: { type: String, unique: true, required: true, lowercase: true, trim: true },
   password: { type: String, required: true },
   admin: { type: Boolean, default: false },
   kc: { type: Number, default: 0 },
